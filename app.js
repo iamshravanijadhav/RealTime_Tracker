6ab71da5b1c0adfd61420cf867efd8ce8b15b037
Shravani <jadhavshravani223@gmail.com>
2026-06-17 10:17:00 +0530

    Update real-time location tracker

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-// socket.io is Js library for realtime web applications. It enables real-time, bidirectional and event-based communication between the browser and the server. It consists of two parts: a client-side library that runs in the browser, and a server-side library for Node.js. Both components have an identical API. Socket.IO primarily uses the WebSocket protocol, but it also provides fallback options for older browsers that do not support WebSockets.
-
-
 const express = require('express');
 const app = express();
 const http = require('http');
 const path = require('path');
 
-const socketio = require('socket.io');
 const server = http.createServer(app);
+const socketio = require('socket.io');
 const io = socketio(server);
-
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function (socket) {
+
+    console.log('User connected:', socket.id);
+
     socket.on('sendLocation', function (data) {
-        io.emit("recieve-location", {id: socket.id, ...data});
+        io.emit('receive-location', {
+            id: socket.id,
+            ...data
+        });
     });
-    
+
     socket.on('disconnect', function () {
-        io.emit("disconnect", socket.id);
+        console.log('User disconnected:', socket.id);
+        io.emit('user-disconnected', socket.id);
     });
+
 });
 
-app.get("/", function (req, res) {
-    res.render("index");
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,52 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position) => {
-        const {latitude, longitude} = position.coords;
-        socket.emit('sendLocation', {latitude, longitude});
-    }, (error) => {console.log(error);
-        }, 
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+
+            socket.emit("sendLocation", {
+                latitude,
+                longitude
+            });
+        },
+        (error) => {
+            console.log(error);
+        },
         {
             enableHighAccuracy: true,
             timeout: 5000,
             maximumAge: 0
-
         }
     );
 }
 
-const map = L.map("map").setView([0, 0], 16);
+const map = L.map("map").setView([25.150776, 75.837232], 40);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Diparch"
+    attribution: "OpenStreetMap"
 }).addTo(map);
 
 const markers = {};
 
-socket.on("recieve-location", (data) => {
-    const {id, latitude, longitude} = data;
-    map.setView([latitude, longitude], 10);
-    if(markers[id]){
+socket.on("receive-location", (data) => {
+
+    const { id, latitude, longitude } = data;
+
+    map.setView([latitude, longitude], 16);
+
+    if (markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
     } else {
         markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
 });
 
-socket.on("disconnect", (id) => {
-    if(markers[id]){
+socket.on("user-disconnected", (id) => {
+
+    if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
     }
-}); 
+
+});
